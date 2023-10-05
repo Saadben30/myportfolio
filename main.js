2b05d69409d3a11b7ecec7a4f3de525010c84870
saad <saadb4997@gmail.com>
2023-10-05 10:18:24 +0200

    modification in java script code

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-let name = 'ilove oumaima'
-console.log(name.slice(-7))
+// Change the title after 2 seconds
+setTimeout(function() {
+    document.body = "New Title";
+}, 2000); // 2000 milliseconds = 2 seconds
